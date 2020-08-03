f3384d68b5f20e063d3fafdfc79181ddcae74c51
rockyFierro <finnslighthouse@gmail.com>
2020-08-02 20:27:23 -0700

    mvp yay, now to spend the rest of the night getting used to drilling...

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,37 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components"
+
+const Button = styled.button`
+  background: #000;
+  color: #727bf7;
+  border: none;
+  border-top-right-radius: 3ch;
+  border-bottom-left-radius:2ch;
+  width:25ch;
+  margin: 1ch 0;
+`
+
+
+
+const Character = ({names}) => {
+
+
+  return(
+  names.map((name, index)=>{
+    return (
+    <div key={index}>
+      <Button>
+        <h2>{name}</h2>
+        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index+1}.png`}
+        alt={`such a cool pokemon`}/>
+      </Button>
+    </div>
+    )
+  })
+  )
+
+};
+
+export default Character;
+
